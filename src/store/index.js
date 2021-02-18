187750ca00f53af5580b767814906ba5951d7e1b
berndengels <engels@f50.de>
2021-02-18 07:48:57 +0100

    stuff

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
+import '@/plugins/axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+
+const apiRoute = "/api/todos",
+	authToken = localStorage.getItem('user.token'),
+	axios = Vue.axios;
 
 export default new Vuex.Store({
 	state: {},
