1bc49ca33cb5351fb996c16ea6d03ee93554e649
codeamazone <e.niggeme@gmail.com>
2021-02-18 15:35:38 +0100

    update

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,28 +1,28 @@
 <template>
-    <div>
-        <h3>Todos</h3>
-        <AddTodo />
-        <hr/>
-<!--        Binde Todo mit props todos-Datenobjekt und handleRemove-Funktion ein-->
-        <Todo :todos="todos" :handleRemove="remove"/>
-    </div>
+  <div>
+    <h3>Todos</h3>
+    <AddTodo :handleAdd="add"/>
+    <hr/>
+    <!--        Binde Todo mit props todos-Datenobjekt und handleRemove-Funktion ein-->
+    <Todo :handleRemove="remove" :todos="todos"/>
+  </div>
 </template>
 
 <script>
 import AddTodo from "./AddTodo";
 import Todo from "./Todo";
-import { data } from "../store/todos";
+import {data} from "../store/todos";
 
 export default {
-    name: "Todos",
-    components: {Todo, AddTodo},
-    created() {
-    },
-    data() {
-        return {
-            todos: data,
-        }
-    },
+  name: "Todos",
+  components: {Todo, AddTodo},
+  created() {
+  },
+  data() {
+    return {
+      todos: data,
+    }
+  },
   methods: {
     remove(todo) {
       // Durchlaufe mit filter das gesamte array und gib alle items zurück,
@@ -35,10 +35,17 @@ export default {
       //     return item
       //   }
       // })
-      },
-    // add(text) {
-    //
-    // },
+    },
+    add(txt) {
+      // Neu angelegtes todo-Objekt analog der todos-Objektstruktur mit neuem text
+      var obj = {
+        id: 100,
+        done: false,
+        text: txt,
+      }
+      // Füge todo-Objekt das neu kreierte Objekt mit push hinzu
+      this.todos.push(obj)
+    },
     // update(todo) {
     //
     // }
