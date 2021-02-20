61d11540c1c8c640236cbe21541f481be403b29c
berndengels <engels@f50.de>
2021-02-20 12:21:08 +0100

    stuff

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
         <h3>Login</h3>
+        <b-form @submit.prevent>
+            Email: <input type="email" required>
+            <br>
+            Passwort: <input type="password" required>
+            <br>
+            <b-button>Login</b-button>
+        </b-form>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Login"
+    name: "Login",
+    data() {
+        return {
+            user: {
+                email: null,
+                password: null,
+            }
+        }
+    }
 }
 </script>
 
